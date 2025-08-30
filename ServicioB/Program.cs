75f9e7e7c4b7f4574b9e9caa66914dab030f58fe
raul <raulalbertoaguilar01@gmail.com>
2025-08-30 02:32:46 -0400

    quitando lo que esta demas

--- ServicioB/Program.cs
+++
@@ -1,7 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet("/carreras", () => "Todas las carreras disponibles ");
-app.Urls.Add("http://0.0.0.0:5000");
-
-app.Run();
